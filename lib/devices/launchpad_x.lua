528861c35394683738d535ca796b4357238c1f25
Ben <bontric@riseup.net>
2021-03-12 17:24:27 +0700

    Automatically switch Launchpad X to programmer mode

--- lib/devices/launchpad_x.lua
+++ lib/devices/launchpad_x.lua
@@ -2,4 +2,16 @@ local launchpad = include('midigrid/lib/devices/launchpad_rgb')
 
 launchpad.device_name = 'launchpad_x'
 
+-- https://llllllll.co/t/how-do-i-send-midi-sysex-messages-on-norns/34359/14
+function launchpad:init()
+  print('Setting Launchpad Programmer mode')
+  m = midi.devices[launchpad.midi_id]
+  d = {0x0,0x20,0x29,0x02,0x0D,0x00,0x7F}
+  m:send{0xf0}
+  for i,v in ipairs(d) do
+    m:send{d[i]}
+  end
+  m:send{0xf7}
+end
+
 return launchpad
