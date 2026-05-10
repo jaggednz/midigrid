7f8a44476b20bd05dd1ba7d8041eec010e431589
Alexei Loghinov <nvmker@gmail.com>
2026-05-11 00:50:32 +0300

    disable rotation for Launchpad Pro MK3 as second device
    Unlike Launchpad X and Mini MK3 where the grid is offset to one side,
    the Launchpad Pro MK3 has its grid perfectly centered, so there is no
    reason to rotate it when used as the second device in a 128 layout.

--- lib/devices/launchpad_pro_mk3.lua
+++ lib/devices/launchpad_pro_mk3.lua
@@ -3,6 +3,8 @@ local launchpad = include('midigrid/lib/devices/launchpad_rgb')
 --Put the device into programmers mode
 launchpad.init_device_msg = { 0xf0,0x00,0x20,0x29,0x02,0x0e,0x0e,0x01,0xf7 }
 
+launchpad.rotate_second_device = false
+
 launchpad.aux.col = {
   {'cc', 89, 0},
   {'cc', 79, 0},
